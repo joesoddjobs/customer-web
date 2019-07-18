ab2f16a8ec38583bc0dab1700dc5ac33ba2c67d0
docovarr2020 <diego_covarrubias@college.harvard.edu>
2019-07-18 17:10:00 -0400

    Config(Initial Auth) -- Set up the initial version of the Auth Stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React from 'react'
+import Amplify from 'aws-amplify'
+import {
+  ConfirmSignIn,
+  ConfirmSignUp,
+  ForgotPassword,
+  RequireNewPassword,
+  SignUp,
+  VerifyContact,
+  withAuthenticator
+} from 'aws-amplify-react'
+// eslint-disable-next-line camelcase
+import aws_exports from './aws-exports'
 
-function App() {
-  return <div className="App">APP</div>;
+Amplify.configure(aws_exports)
+
+const MySignIn = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'red'
+      }}
+    />
+  )
 }
 
-export default App;
+function App() {
+  return <div className="App">APP</div>
+}
+
+export default withAuthenticator(App, false, [
+  <MySignIn />,
+  <ConfirmSignIn />,
+  <VerifyContact />,
+  <SignUp />,
+  <ConfirmSignUp />,
+  <ForgotPassword />,
+  <RequireNewPassword />
+])
