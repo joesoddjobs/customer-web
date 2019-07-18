51ba185358606e85e001a2db2c18c757e734844d
docovarr2020 <diego_covarrubias@college.harvard.edu>
2019-07-18 17:10:31 -0400

    Styling(Auth) -- Initial versions of the auth containers

---
+++ src/containers/auth/ConfirmSignIn/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ConfirmSignIn = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'red'
+      }}
+    />
+  )
+}
+
+export default ConfirmSignIn
---
+++ src/containers/auth/VeriftyContact/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const VerifyContact = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'red'
+      }}
+    />
+  )
+}
+
+export default VerifyContact
