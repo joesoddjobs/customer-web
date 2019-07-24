4a872535ea9ea57acc80a3f0de1c823d9eb67f9d
docovarr2020 <diego_covarrubias@college.harvard.edu>
2019-07-24 18:39:08 -0400

    Styling(Home Page) -- Add the initial version of the Home Page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Container } from './styles'
+import {
+  AboutUsBlock,
+  BookAJobBlock,
+  IntroBlock,
+  JobTypesBlock,
+  PriceBlock,
+  TestimonialBlock
+} from './components'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <Container>
+      <IntroBlock />
+      <JobTypesBlock />
+      <PriceBlock />
+      <AboutUsBlock />
+      <TestimonialBlock />
+      <BookAJobBlock />
+    </Container>
+  )
 }
 
 export default Home
