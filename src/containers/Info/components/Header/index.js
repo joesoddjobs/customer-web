f058cb8c90e75549fc0817e1ef40e737694200a8
docovarr2020 <diego_covarrubias@college.harvard.edu>
2019-07-27 16:54:15 -0400

    Styling(Info Header) -- Add the intitial version of the Header for the Info page

---
+++ src/containers/Info/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  Container,
+  HeaderText,
+  BodyText,
+  BookAJobButton,
+  Image
+} from './styles'
+
+const Header = () => {
+  return (
+    <Container>
+      <HeaderText>How It Works</HeaderText>
+      <BodyText>
+        Joe&apos;s Odd Jobs utilizes high school and college students to help
+        customers fufill jobs they may not have time for, may not know how to
+        do, or just don’t want to do at all
+      </BodyText>
+      <BookAJobButton>Book a Job</BookAJobButton>
+      <Image />
+    </Container>
+  )
+}
+
+export default Header
