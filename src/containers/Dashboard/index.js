f2100f588661a29b8e959f4762468bed080cb257
docovarr2020 <diego_covarrubias@college.harvard.edu>
2019-07-27 15:19:56 -0400

    Styling(Dashboard) -- Add the initial version of the Job Dashboard page

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import { Auth } from 'aws-amplify'
-
-const click = e => {
-  e.preventDefault()
-  Auth.signOut()
-}
+import Header from './components/Header'
+import Table from './components/JobsTable'
+import { Container, TablesContainer } from './styles'
 
 const Dashboard = () => {
-  console.log(Auth)
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: '100%',
-        height: '100vh',
-        backgroundColor: 'blue'
-      }}
-    >
-      <button onClick={e => click(e)} type="button">
-        SignOut
-      </button>
-    </div>
+    <Container>
+      <Header />
+      <TablesContainer>
+        <Table title="Scheduled Jobs" />
+        <Table title="Past Jobs" past />
+      </TablesContainer>
+    </Container>
   )
 }
 
---
+++ src/containers/Dashboard/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { Flex } from '@rebass/grid'
+
+export const Container = styled(Flex)`
+  flex-direction: column;
+  width: 100%;
+`
+export const TablesContainer = styled(Flex)`
+  flex-direction: column;
+  width: 100%;
+  padding: 50px 50px 0px 50px;
+`
